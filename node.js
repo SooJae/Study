1359027e41c35b1295acd6f3353fa3862f998e68
SooJae <43255680+SooJae@users.noreply.github.com>
2018-09-20 16:56:45 +0900

    Update node.js

--- node.js
+++ node.js
@@ -4,3 +4,24 @@ HTML                   JavaScript
 WEB Browser            node.js runtime
 
 
+
+
+var http = require('http');
+var fs = require('fs');
+
+var app = http.createServer(function(request,response){
+    var url = request.url;
+    if(request.url == '/'){
+      url = '/index.html';
+    }
+    if(request.url == '/favicon.ico'){
+      return response.writeHead(404);
+    }
+    response.writeHead(200);
+    console.log(__dirname+url);
+    response.end(fs.readFileSync(__dirname + url)); // 이 부분에따라 사용자에게 전송하는 데이터가 바뀐다.
+
+
+});
+
+app.listen(3000);
