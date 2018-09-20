5a0a7f4a9f79e64151836444bab3f0282e231c20
SooJae <43255680+SooJae@users.noreply.github.com>
2018-09-20 18:01:47 +0900

    Update node.js

--- node.js
+++ node.js
@@ -25,3 +25,18 @@ var app = http.createServer(function(request,response){
 });
 
 app.listen(3000);
+
+
+줄바꿈 하려면 \n\ 
+\n\을 해주면 된다. 근데 굉장히 불편..
+그래서 template literal을 이용한다.
+
+`을 이용
+var letter =`Lorem ipsum ${name}
+
+dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut ${name}labore et dolore magna aliqua. 
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex e'+name+'a commodo consequat. 
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla paria${name}tur. 
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
+
+
