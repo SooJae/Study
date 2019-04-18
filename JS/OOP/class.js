f29eed5ddb29d3a53bac7ccb0592ec94775ed605
SooJae <mynameisleesujae@gmail.com>
2019-04-18 21:41:51 +0900

    study javascript

---
+++ JS/OOP/class.js
@@ -0,0 +1,19 @@
+class Person{
+
+    constructor(name,first,second){
+        this.name=name;
+        this.first=first;
+        this.second= second;
+    }
+}
+var kim = new Person('수재',10,20);
+console.log('kim',kim)
+
+
+// function Person(name,first,second,third){
+//     this.name=name;
+//     this.first=first;
+//     this.second= second;
+//     this.third= third;
+    
+// }
