885af846573cb00b81457fb47c8e8d25c344df38
SooJae <Mynameisleesujae@gmail.com>
2019-08-30 11:37:28 +0900

    study

---
+++ JS/gulp/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "comma-dangle":[
+            2,
+            "always-multiline"
+        ],
+        "indent":[
+            2,
+            4
+        ],
+    }
+};
