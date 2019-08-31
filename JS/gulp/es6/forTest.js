6fec186f5dba4652c43a8a3e230cb3a6d64db334
SooJae <mynameisleesujae@gmail.com>
2019-08-31 20:09:27 +0900

    study

---
+++ JS/gulp/es6/forTest.js
@@ -0,0 +1,10 @@
+// const player = {name : 'Thomas', rank :'Midshipman',age:25};
+
+// for(let prop in player){
+//     if(!player.hasOwnProperty(prop)) continue;
+//     console.log(prop + ': ' + player[prop]);
+// }
+
+const hand = [4, 5, 6];
+for(let face of hand)
+ console.log(`you rolled ... ${face}`);
