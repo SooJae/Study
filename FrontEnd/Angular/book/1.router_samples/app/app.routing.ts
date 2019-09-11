b01081f7d573897bdb849e32d0e45c92c2690cea
SooJae <Mynameisleesujae@gmail.com>
2019-09-11 15:17:15 +0900

    study

--- FrontEnd/Angular/book/1.router_samples/app/app.routing.ts
+++ FrontEnd/Angular/book/1.router_samples/app/app.routing.ts
@@ -1,9 +1,10 @@
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './components/home.component';
-import { ProductDetailComponent } from './components/product.component';
+import { Routes, RouterModule } from '@angular/router'; //Routes RouterModule을 불러온다.
+import { HomeComponent } from './components/home.component'; // 기본 URL이면 HomeComponent를 렌더링한다.
+import { ProductDetailComponent } from './components/product.component'; 
 
 const routes : Routes = [
-	{ path : '', component : HomeComponent },
-	{ path : 'product', component : ProductDetailComponent }
+	{ path : '', component : HomeComponent },			// 기본 URL 뒤에 product 주소가 붙으면 ProductDetailComponent를 렌더링한다.
+	{ path : 'product', component : ProductDetailComponent } // 라우터 설정을 모듈 외부로 공개해서 루트 모듈에서 사용할 수 있도록 한다.
 ];
 export const routing = RouterModule.forRoot(routes);
+
