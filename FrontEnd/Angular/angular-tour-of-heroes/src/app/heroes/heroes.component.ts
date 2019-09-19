93833a2be0b96ddb4b0706218dab56555fb27f61
SooJae <Mynameisleesujae@gmail.com>
2019-09-20 00:18:51 +0900

    study

--- FrontEnd/Angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ FrontEnd/Angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from "../mock-heroes";
-
+import { HeroService } from "../hero.service";
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -9,16 +9,23 @@ import { HEROES } from "../mock-heroes";
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  // heroes = HEROES;
+  // 관심사의 분리
+  heroes : Hero[];
+
   selectedHero: Hero;
-
-
-  constructor() { }
+// 이렇게 작성하면 heroService 인자를 클래스 프로퍼티로 선언하면서 HeroService 타입의 의존성 객체가 주입되기를 요청한다는 것을 의미합니다.
+// 그러면 Angular가 HeroesComponent를 생성할 때 의존성 주입 시스템이 HeroService의 인스턴스를 찾아서 heroService 라는 인자로 전달할 것입니다.
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
   }
 
   onSelect(hero: Hero)  {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 }
