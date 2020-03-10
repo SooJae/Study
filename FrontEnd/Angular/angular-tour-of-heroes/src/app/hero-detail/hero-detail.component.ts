66db16c5e9fe7b048ae975736029bf200e8ac8ed
soojae <developersoojae@gmail.com>
2020-03-11 00:50:15 +0900

    공부 : switchMap

--- FrontEnd/Angular/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ FrontEnd/Angular/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,26 +1,68 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
 
-import { Hero }         from '../hero';
-import { HeroService }  from '../hero.service';
+import {Hero} from '../hero';
+import {HeroService} from '../hero.service';
+import {combineLatest, from, fromEvent, interval, Observable, Subject} from "rxjs";
+import {map, mapTo, startWith, switchMap, tap} from "rxjs/operators";
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: [ './hero-detail.component.css' ]
+  styleUrls: ['./hero-detail.component.css']
 })
-export class HeroDetailComponent implements OnInit {
+export class HeroDetailComponent implements OnInit, AfterViewInit {
   hero: Hero;
+
+  @ViewChild('go', {static: false})
+  public go: ElementRef;
+
+  @ViewChild('back', {static: false})
+  public back: ElementRef;
+  private go$: Subject<any>;
+  private back$: Observable<any>;
+
+  public n: number = 1;
 
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
-    private location: Location
-  ) {}
+    private location: Location,
+
+  ) {
+  }
 
   ngOnInit(): void {
     this.getHero();
+  }
+
+  ngAfterViewInit() {
+    this.go$ = interval(1000)
+      .pipe(
+        map(v => v)
+      );
+    this.back$ = fromEvent(this.back.nativeElement, 'click').pipe(
+      mapTo('수재'),
+    );
+
+    this.back$.pipe(
+      switchMap(v => this.go$),
+      tap(v => console.log(v)),
+    ).subscribe(
+      console.log
+    );
+
+    // switchMap 쓰고 tap에다 검색어 저장
+
+    // combineLatest(this.go$, this.back$).pipe(
+    //   map(data => console.log(data))
+    // ).subscribe();
+  }
+
+  clearValue() {
+    console.log('안녕');
+    this.n = 0;
   }
 
   getHero(): void {
@@ -32,4 +74,6 @@ export class HeroDetailComponent implements OnInit {
   goBack(): void {
     this.location.back();
   }
+
+
 }
